f2331a6112538b02c419ffa40683a7501472bf7f
Arun Shankar <arun@lemorian.tech>
2021-07-13 15:53:41 +0530

    faq  changes

--- components/faqs/FaqItem.jsx
+++ components/faqs/FaqItem.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function FaqItem() {
+export default function FaqItem({ post }) {
   const [expand, setExpand] = useState(false);
 
   return (
@@ -8,7 +8,7 @@ export default function FaqItem() {
       <button
         className='bg-gray-300 text-gray-700 font-semibold p-2 rounded inline-flex justify-between min-w-full'
         onClick={() => setExpand((_expand) => !_expand)}>
-        <span className='ml-3 '>What is yiu academy ?</span>
+        <span className='ml-3 '>{post.q}</span>
         <svg
           className='fill-current h-4'
           xmlns='http://www.w3.org/2000/svg'
@@ -18,15 +18,12 @@ export default function FaqItem() {
       </button>
       <ul
         className='text-gray-700 transition-height duration-1000 ease-in-out overflow-hidden'
-        style={expand ? { height: '0px' } : { height: 'auto' }}>
+        style={!expand ? { height: '0px' } : { height: 'auto' }}>
         <li>
           <a
             className='bg-gray-200 px-2 py-2 block whitespace-no-wrap'
             href='#'>
-            Yiuacademy is a marketplace and online learning portal for kids aged
-            2 till 22. We help parents find great classes for their kids. And we
-            help teachers to promote their classes to parents. We offer both
-            recorded video classes and live classes from teachers.
+            {post.a}
           </a>
         </li>
       </ul>
--- pages/faqs.jsx
+++ pages/faqs.jsx
@@ -1,18 +1,34 @@
 import Main from '~/layouts/main';
-import '../styles/Home.module.css';
-import FaqDropdown from '~/components/faqDropdown';
+import data from '../mock/faq.json';
+import FaqItem from '../components/faqs/FaqItem';
 
-export default function Home(props) {
+export default function Faqs({ posts }) {
   return (
     <Main>
       <div className='m-auto flex flex-col justify-center'>
         <h1 className='text-purple-600  text-4xl font-headline font-sans font-extrabold mt-20 flex justify-center'>
           Frequently Asked Questions (FAQs)
         </h1>
-        <FaqDropdown />
+        <div className='flex justify-center px-10 py-15 min-w-full mt-3 '>
+          <div className='px-10 py-15 min-w-full '>
+            <div className='group inline-block relative min-w-full'>
+              {posts.map((post) => {
+                return <FaqItem post={post} key={post.id} />;
+              })}
+            </div>
+          </div>
+        </div>
       </div>
       <br></br>
       <br></br>
     </Main>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
