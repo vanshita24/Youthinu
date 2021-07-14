d64382297d1bc6246e4bda658cd08d4c0f41fc31
SWATI.SINGH2019 <9667522@gmail.com>
2021-07-15 00:53:33 +0530

    msg

--- pages/liveAcademy/school.js
+++ pages/liveAcademy/school.js
@@ -41,6 +41,106 @@ export default function ThirdPost() {
           </div>
         </div>
       </div>
+      <div className='grid  md:grid-cols-2 gap-10 sm:grid-cols-1 max-w-5xl m-auto bg-purple-100 overflow-hidden shadow-md rounded-lg mb-12 p-5 justify-center items-center '>
+        <img
+          src='/images/school.jpeg'
+          alt=''
+          className='w-full h-64 object-center rounded-lg shadow-lg'
+        />
+
+        <div className='p-8'>
+          <div>
+            <p className='text-gray-700 text-base font-bold justify-center line-clamp-6 '>
+              <h1 className='text-purple-500 mt-6 text-2xl font-headline tracking-tight font-bold leading-snug '>
+                CODING AND LOGICAL MATH CLASSES FOR SCHOOLS
+              </h1>
+              <br></br>
+              One of the most vital objectives in education is to prepare
+              students for their future careers. Over the last two decades,
+              coding and programming have emerged as some of the most desirable
+              skills for employers. To adapt to changes in the landscape of the
+              jobs market, schools should adjust their curriculum and
+              incorporate coding. Schools have a responsibility to provide their
+              students with all the tools and skills they need to succeed in the
+              modern workplace. Increasingly, that means an ability to code. By
+              incorporating coding into the curriculum, you hand children an
+              essential tool for building a successful career. Introductory
+              programming classes in schools can help children understand
+              programming structure basics, logic, and design. Of course, not
+              all children will become software engineers; however, learning
+              programming may help sharpen their practical and logical thinking
+              skills. Most experts emphasize the importance of teaching coding
+              to school students. Coding gives you a competitive advantage. If
+              you want your kids to be more desirable in the eyes of admission
+              officers and future employers, start teaching them to code. It is
+              one hot skill that many people lack and will give your children
+              the much-desired competitive edge when applying to colleges,
+              internships, and jobs. Coding helps kids and teens to learn and
+              understand how to approach various challenges and difficulties
+              using logic and computational thinking. This logical thinking is
+              powerful and will help children a lot in school, work, and life.
+              In addition to computational thinking and programming skills,
+              coding fosters executive functioning skills, such as
+              problem-solving, planning, and mathematical thinking. Coding
+              equips kids with a skill set that will assist them down the
+              academic road. A child effectively learns languages when he is
+              young, and learning programming syntax is akin to learning a new
+              language. Early exposure will shape how he thinks and makes
+              computational thinking second nature. At the age of 7 or 8, most
+              kids would have sufficiently developed the logical and critical
+              thinking skills needed for coding. Kids as young as ten years can
+              produce programs that achieve professional quality. If a kid
+              starts learning how to code from an early age, it opens the door
+              to plenty of opportunities - acquiring life skills and exploring
+              career opportunities. As you can see, the benefits of coding for
+              students and children, in general, go well beyond learning how to
+              program a computer. Learning to code gives children the advantage
+              and is a highly valued skill that can help children throughout
+              their day-to-day lives. Kids that learn to code from a young age
+              are going to be the pioneers of the future. Your child could go on
+              to change lives by working on apps, robotics, and other computing
+              programs that help to shape our future for the better in the
+              education, health, and energy sectors. Besides coding, logical
+              math plays an indispensable role in molding the career of
+              students. For making your children succeed in life, you must
+              develop their mathematical thinking and creative reasoning skills.
+              It might sound challenging at first, but with good teaching and
+              mentoring, it is indeed possible. The development of such skills
+              will have significant positive effects on your child. The
+              mathematical critical thinking abilities will enable them to
+              understand their math lessons faster. Instead of blindly
+              memorizing the formulae and concepts, students would start
+              appreciating the topics and remember them for a longer time. It
+              will result in a significant improvement in their math skills.
+              Once your children develop the necessary mathematical thinking and
+              creative reasoning skills, they will correlate theory with things
+              and situations that they encounter around them regularly. It will
+              make math learning much more fun for them, and they will also
+              acquire the habit of doing mental math, which will be effective in
+              any field that they will pursue in the future. Start teaching
+              logical math to your children from a young age, and they will reap
+              the benefits forever. These skills make your children stand out
+              from the crowd and are stepping stones in acing competitive exams
+              in later stages of life. Moreover, it would be difficult for
+              working parents to drop and pick up their kids from various
+              coaching classes amidst their hectic schedules. So, if both
+              logical math and coding are covered effectively in the school
+              itself, parents' workload gets reduced, and they feel more
+              satisfied with their kid's education. YouthInU Live Academy is
+              ready to take Logical Math and Coding classes to young school
+              students and inspire them. We have designed our sessions to give
+              kids a fun, interactive, and innovative program; it will not only
+              educate your child but keep them occupied and fuel a passion for
+              tech. We conduct student-friendly mathematical and coding classes
+              to aid students in their academics and skill development. We are a
+              one-stop solution for kids to develop multiple skills. It surely
+              could be the start of a bright future for your children. Feel free
+              to contact us if you need any further information!
+            </p>
+          </div>
+        </div>
+      </div>
+
       <br></br>
       <br></br>
       <br></br>
