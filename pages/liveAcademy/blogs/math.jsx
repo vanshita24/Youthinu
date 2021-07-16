727b7fc01e94797e60e79b33163b6a576b7055ca
Vanshita24 <khilranivanshita@gmail.com>
2021-07-16 12:40:26 +0530

    **

---
+++ pages/liveAcademy/blogs/math.jsx
@@ -0,0 +1,87 @@
+import Main from '~/layouts/main';
+//import "../styles/Home.module.css";
+
+export default function Math() {
+  return (
+    <Main>
+      <div class='p-8'>
+        <div>
+          <h1 className='text-purple-700 font-extrabold text-xl mt-5 text-center text-3xl'>
+            LOGICAL MATH
+          </h1>
+        </div>
+        <div class='uppercase tracking-wide text-sm text-red-500 font-semibold text-center mt-5 text-2xl'>
+          Go down deep enough into anything and you will find Mathematics{' '}
+          <div className='camelcase font-bold'>Dean Schlicter</div>
+        </div>
+
+        <p class='mt-2 text-gray-700 text-xl'>
+          <div>
+            Experts say that Mathematics is the gate and key of Science. All
+            these Sciences such as Physics, Medicine, and Engineering progress
+            only with the aid of Mathematics. So it is aptly remarked,
+            “Mathematics is a Science of all Sciences and art of all arts.” It
+            is much more than Arithmetic, more than Algebra more than Geometry.
+            Also, it is much more than Trigonometry, Statistics, and Calculus.
+            Primarily mathematics is a way of thinking and organizing logical
+            proofs. Mathematics intimately connects with everyday life and is
+            necessary to the successful conduct of affairs. We use math every
+            day. For example, we use math when crossing the road, cooking,
+            telling the time, and many more.
+          </div>
+           
+          <div className=''>
+            Mathematics makes our life orderly and prevents chaos. Certain
+            qualities nurtured by mathematics are the power of reasoning,
+            creativity, abstract or spatial thinking, critical thinking,
+            problem-solving ability, and even efficient communication skills.
+          </div>
+          <div className='mt-5'>
+            Mathematics is the cradle of all creations, without which the world
+            cannot move an inch. Everyone needs mathematics in their day-to-day
+            life, be it a cook or a farmer, a carpenter or a mechanic, a
+            shopkeeper or a doctor, an engineer or a scientist, a musician or a
+            magician. Even insects use mathematics in their everyday life for
+            existence. Snails make their shells, spiders design their webs, and
+            bees build hexagonal combs. There are countless examples of
+            mathematical patterns in nature’s fabric.{' '}
+            <b>
+              To live in a mathematically-driven world and not know math is like
+              walking through an art museum with your eyes closed.
+            </b>
+          </div>
+          <div className='mt-5'>
+            Anyone can be a mathematician if one is given proper guidance and
+            training in the formative period of one’s life. A good curriculum of
+            mathematics is helpful in effective teaching and learning of the
+            subject. Experience says it is possible to learn mathematics more
+            easily and enjoyably if our curriculum includes mathematical
+            activities and games. Math puzzles and riddles encourage and attract
+            an alert and open-minded attitude among youngsters and help them
+            develop clarity in their thinking.{' '}
+          </div>
+          <div className='mt-5'>
+            Mathematical concepts should be taught clearly to a child, right
+            from their primary classes. The process of learning is complete if
+            our sense of hearing accompanies our sense of sight. YouthInU
+            Academy brings forward a collection of comprehensive mathematics
+            courses for students from KG till Class 12. Learn Math with us
+            through puzzles, riddles, illustrations, sketches, models, and
+            whatnot. Let us realize and appreciate the beauty of the subject and
+            embrace it with all our hearts. It is a talent to be compulsorily
+            honed by all in every walk of life.
+          </div>{' '}
+          <div className='font-bold text-green-500 mt-2'>
+            <b>Multiply your success by having Math in your Path..!!!!</b>
+          </div>
+        </p>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </Main>
+  );
+}
